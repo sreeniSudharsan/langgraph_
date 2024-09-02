a2e961708e40c1bcda72101f7cd1da511a35e6e5
Sreenivasan R S <srrs@deloitte.com>
2024-09-02 16:59:58 +0530

    Add Simple Rag Fixed

--- simple_rag_application.py
+++ simple_rag_application.py
@@ -9,7 +9,6 @@ import json
 from langchain_core.messages import ToolMessage
 from typing import Any, Literal
 
-
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
@@ -24,10 +23,38 @@ def chatbot(state: State):
     return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 graph_builder.add_node("chatbot", chatbot)
+import json
+
+from langchain_core.messages import ToolMessage
 
 
-tool_node = BasicToolNode(tools = [tool])
+class BasicToolNode:
+    """A node that runs the tools requested in the last AIMessage."""
 
+    def __init__(self, tools: list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, inputs: dict):
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
+
+
+tool_node = BasicToolNode(tools=[tool])
 graph_builder.add_node("tools", tool_node)
 
 def route_tools(state: State, )-> Literal["tools", "___end__"]:
@@ -70,6 +97,4 @@ while True:
         print("Goodbye!")
         break
     for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            if isinstance(value["messages"][-1], BaseMessage):
-                print("Assistant:", value["messages"][-1].content)
+        event["messages"][-1].pretty_print()
