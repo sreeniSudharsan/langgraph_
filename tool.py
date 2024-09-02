d3e12c92ae7f1299812e14b86ef9f26a8a793eca
Sreenivasan R S <srrs@deloitte.com>
2024-09-02 15:08:02 +0530

    Add Simple Rag Applicaiton Complete

--- tool.py
+++ tool.py
@@ -1,18 +1,23 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain.tools import tool
-from typing import Optional
+#Custom Tooling. Need to test it out later
 
-@tool
-def search_tool(input: Optional[str]):
-    if input is None:
-        return 'Sorry I did not get that! Can you please type in again'
-    tool = TavilySearchResults(max_results=2)
-    return tool.invoke(input)
+# from langchain_community.tools.tavily_search import TavilySearchResults
+# from langchain.tools import tool
+# from typing import Optional
+
+# @tool
+# def search_tool():
+#     '''    
+#     Tool for searching the web using TavilySearchResults. Use this wisely
+#     '''
+#     if input is None:
+#         return 'Sorry I did not get that! Can you please type in again'
+#     tool = TavilySearchResults(max_results=2)
+#     return tool
 
 
-#tool - testing
-if __name__ == "__main__":
-    result = search_tool("What's a node in langgraph?")
-    for object in result:
-        url, content = object['url'], object['content']
-        print(f'"URL": {url} \n "Content": {content}')
+# #tool - testing
+# if __name__ == "__main__":
+#     result = search_tool.invoke("What's a node in langgraph?")
+#     for object in result:
+#         url, content = object['url'], object['content']
+#         print(f'"URL": {url} \n "Content": {content}')
